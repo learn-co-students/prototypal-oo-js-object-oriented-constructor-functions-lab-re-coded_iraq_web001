bbde40e3e5b3056a8317232db3b8663ed18d72ce
zena abd alkarem alhello <zena34269@gmail.com>
2020-08-07 20:36:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+Function Scooter(year,color,model)
+{
+ this.year=year;
+ this.color=color;
+ this.model=model;
+}
+Function Driver(name,age,experience){
+   this.name=name;
+ this.age=age;
+ this.experience=experience;
+}
+Function PickupLocation (address,city){
+  this.address=address;
+  this.city=city;
+}
